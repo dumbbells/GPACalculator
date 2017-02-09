f481c8ba81c886e092f682481943235359e72ea9
Bill Moll <thsmrtone1@gmail.com>
2017-02-09 15:39:39 -0600

    Started a react app

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
+import App from './App';
+import './style.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
