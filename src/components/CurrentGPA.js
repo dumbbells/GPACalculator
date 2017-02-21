4ad8a5b44b12db4117908847463d1c74636d7675
Bill Moll <thsmrtone1@gmail.com>
2017-02-21 13:18:36 -0600

    minor style changes

--- src/components/CurrentGPA.js
+++ src/components/CurrentGPA.js
@@ -4,18 +4,14 @@ class CurrentGPA extends Component {
     render() {
         return (
             <div>
-                <form className="form-horizontal">
+                <form className="form-inline">
                     <div className="form-group">
-                        <label htmlFor="current-gpa" className="col-sm-3 control-label">Current GPA</label>
-                        <div className="col-sm-9">
-                            <input type="text" className="form-control" id="current-gpa" />
-                        </div>
+                        <label htmlFor="current-gpa" className="control-label side-padding">Current GPA</label>
+                        <input type="text" className="form-control" id="current-gpa" />
                     </div>
                     <div className="form-group">
-                        <label htmlFor="units-attempted" className="col-sm-3 control-label">Units Attempted</label>
-                        <div className="col-sm-9">
-                            <input type="text" className="form-control" id="units-attempted" />
-                        </div>
+                        <label htmlFor="units-attempted" className="control-label side-padding">Units Attempted</label>
+                        <input type="text" className="form-control" id="units-attempted" />
                     </div>
                 </form>
             </div>
