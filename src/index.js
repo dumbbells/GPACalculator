1cb252ad20c7ba6927b413ef4695841ab98962db
Bill Moll <thsmrtone1@gmail.com>
2017-02-22 12:07:03 -0600

    fixed warning for unused jquery include in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 
