4af56b505eec6fddffeff80b612cb7a6dec4f69c
Bill Moll <thsmrtone1@gmail.com>
2017-02-28 13:22:10 -0600

    Bug fix: hours attempted and current gpa didnt update correctly

--- src/components/GpaCalculator.js
+++ src/components/GpaCalculator.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import CurrentGPA from './CurrentGPA';
 import CourseTable from './CourseTable';
 import Course from '../objects/Course';
-import GpaMapping from '../objects/GpaMapping';
 import Results from './Results';
 import Reset from './Reset';
 
@@ -18,15 +17,13 @@ class GpaCalculator extends Component {
             unique_id: 0,
             rows: [],
             currentGpa: 0,
-            unitsAttempted: 0,
-            calculatedGpa: 0
+            unitsAttempted: 0
         };
         
         this.onChange = this.onChange.bind(this);
         this.addCourse = this.addCourse.bind(this);
         this.onCurrentGpaChange = this.onCurrentGpaChange.bind(this);
         this.onUnitsAttemptedChange = this.onUnitsAttemptedChange.bind(this);
-        this.calculate = this.calculate.bind(this);
         this.reset = this.reset.bind(this);
     }
     
@@ -39,11 +36,8 @@ class GpaCalculator extends Component {
             unique_id: this.unique_id,
             rows: [course],
             currentGpa: 0,
-            unitsAttempted: 0,
-            calculatedGpa: 0
+            unitsAttempted: 0
         });
-        
-        this.calculate();
     }
     
     addCourse() {
@@ -89,7 +83,6 @@ class GpaCalculator extends Component {
                 break;
         }
         
-        this.calculate();
     }
     
     onCurrentGpaChange(value) {
@@ -98,60 +91,6 @@ class GpaCalculator extends Component {
     
     onUnitsAttemptedChange(value) {
         this.setState({unitsAttempted: value});
-    }
-    
-    calculate() {
-        var rows = this.state.rows;
-        
-        if(this.state.unitsAttempted === 0 || this.state.currentGpa === 0)
-        {
-            //TODO: zero is an unacceptable check value
-            this.setState({calculatedGpa: "Form needs more information"});
-            return;
-        }
-        
-        var current_hours_attempted = this.state.unitsAttempted;
-        var current_gpa = this.state.currentGpa;
-        var current_grade_points = current_gpa * current_hours_attempted;
-        
-        var total_hours_attempted = 0;
-        var upcoming_grade_points = 0;
-        
-        total_hours_attempted += Number(current_hours_attempted);
-        
-        for(var i = 0; i < rows.length; i++)
-        {
-            if(rows[i].units === undefined || rows[i].projectedGrade === undefined || rows[i].units === 0 || rows[i].projectedGrade === "")
-            {
-                this.setState({calculatedGpa: "Form needs more information"});
-                return;
-            }
-            
-            if(rows[i].repeat === true && (rows[i].previousGrade === undefined || rows[i].previousGrade === ""))
-            {
-                this.setState({calculatedGpa: "Form needs more information"});
-                return;
-            }
-            
-            if(rows[i].repeat)
-            {
-                current_grade_points -= Number(rows[i].units * GpaMapping.getPoints(rows[i].previousGrade));
-            }
-            else
-            {
-                total_hours_attempted += Number(rows[i].units);
-            }
-            
-            upcoming_grade_points += Number(rows[i].units * GpaMapping.getPoints(rows[i].projectedGrade));
-        }
-        
-        var total_grade_points = current_grade_points + upcoming_grade_points;
-        var future_gpa = total_grade_points / total_hours_attempted;
-        
-        
-        if(total_hours_attempted === 0) return;
-        
-        this.setState({calculatedGpa: future_gpa.toFixed(3)});
     }
     
     reset() {
@@ -164,8 +103,7 @@ class GpaCalculator extends Component {
             unique_id: this.unique_id,
             rows: [course],
             currentGpa: 0,
-            unitsAttempted: 0,
-            calculatedGpa: 0
+            unitsAttempted: 0
         });
     }
     
@@ -175,7 +113,7 @@ class GpaCalculator extends Component {
                 <h1>GPA Calculator</h1>
                 <CurrentGPA onCurrentGpaChange={this.onCurrentGpaChange} onUnitsAttemptedChange={this.onUnitsAttemptedChange} />
                 <CourseTable rows={this.state.rows} onChange={this.onChange} addCourse={this.addCourse} />
-                <Results gpa={this.state.calculatedGpa} />
+                <Results rows={this.state.rows} unitsAttempted={this.state.unitsAttempted} currentGpa={this.state.currentGpa} />
                 <Reset onClick={this.reset} />
             </div>
         );
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,12 +1,87 @@
 import React, { Component } from 'react';
+import GpaMapping from '../objects/GpaMapping';
 
 class Results extends Component {
+    
+    constructor(props) {
+        super(props);
+        
+        this.state = {calculatedGpa: ""};
+        
+        this.calculate = this.calculate.bind(this);
+    }
+    
+    componentDidMount() {
+        this.setState({calculatedGpa: ""});
+        this.calculate();
+    }
+    
+    componentDidUpdate(prevProps, prevState) {
+        if(prevProps !== this.props)
+        {
+            this.calculate();
+        }
+    }
+    
+    calculate() {
+        var rows = this.props.rows;
+        
+        if(this.props.unitsAttempted === 0 || this.props.currentGpa === 0)
+        {
+            //TODO: zero is an unacceptable check value
+            this.setState({calculatedGpa: "Form needs more information"});
+            return;
+        }
+        
+        var current_hours_attempted = this.props.unitsAttempted;
+        var current_gpa = this.props.currentGpa;
+        var current_grade_points = current_gpa * current_hours_attempted;
+        
+        var total_hours_attempted = 0;
+        var upcoming_grade_points = 0;
+        
+        total_hours_attempted += Number(current_hours_attempted);
+        
+        for(var i = 0; i < rows.length; i++)
+        {
+            if(rows[i].units === undefined || rows[i].projectedGrade === undefined || rows[i].units === 0 || rows[i].projectedGrade === "")
+            {
+                this.setState({calculatedGpa: "Form needs more information"});
+                return;
+            }
+            
+            if(rows[i].repeat === true && (rows[i].previousGrade === undefined || rows[i].previousGrade === ""))
+            {
+                this.setState({calculatedGpa: "Form needs more information"});
+                return;
+            }
+            
+            if(rows[i].repeat)
+            {
+                current_grade_points -= Number(rows[i].units * GpaMapping.getPoints(rows[i].previousGrade));
+            }
+            else
+            {
+                total_hours_attempted += Number(rows[i].units);
+            }
+            
+            upcoming_grade_points += Number(rows[i].units * GpaMapping.getPoints(rows[i].projectedGrade));
+        }
+        
+        var total_grade_points = current_grade_points + upcoming_grade_points;
+        var future_gpa = total_grade_points / total_hours_attempted;
+        
+        
+        if(total_hours_attempted === 0) return;
+        
+        this.setState({calculatedGpa: future_gpa.toFixed(3)});
+    }
     
     render() {
         return (
             <div className="panel panel-default">
                 <div className="panel-body">
-                    <strong>GPA:</strong> {this.props.gpa}
+                    <strong>GPA:</strong> {this.state.calculatedGpa}
                 </div>
             </div>
         );
